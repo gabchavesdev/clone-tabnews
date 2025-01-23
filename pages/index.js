489d356a4602133ae67aa014637dccde53857499
Gabriel Chaves <120656954+gabchavesdev@users.noreply.github.com>
2025-01-23 20:46:43 +0000

    Atualizando a página inicial

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+function Home() {
+    return (
+        <>
+            <p><em>Lapso e reversão, juntando as duas expressões ilimitadas é possível criar e empurrar uma massa imaginária...</em></p>
+            <hr />
+            <blockquote>
+                <h1>“Vazio Roxo.”</h1>
+            </blockquote>
+            <p><small>— O Honrado.</small></p>
+        </>
+    );
+}
+
+export default Home;
