cecd3ed84a79b854baeeadfd329a9008a00ad3fc
Gabriel Chaves <120656954+gabchavesdev@users.noreply.github.com>
2025-01-24 15:05:00 +0000

    Adicionando emojis

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 function Home() {
     return (
         <>
-            <p><em>Lapso e reversão, juntando as duas expressões ilimitadas é possível criar e empurrar uma massa imaginária...</em></p>
+            <p><b><em>Lapso e reversão, juntando as duas expressões ilimitadas é possível criar e empurrar uma massa imaginária...</em></b></p>
             <hr />
             <blockquote>
-                <h1>“Vazio Roxo.”</h1>
+                <h1>
+                    “Vazio Roxo.” <span>🫱🟣</span>
+                </h1>
             </blockquote>
             <p><small>— O Honrado.</small></p>
         </>
